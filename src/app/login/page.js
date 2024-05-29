bcfdcfbdbf405a3117e7170904e14bfcdedfaa84
ilo80 <ilann.mimet@gmail.com>
2024-05-29 19:33:48 +0200

    Ajout de la page de login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -16,11 +16,23 @@ function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type='text' name='pseudo' placeholder='Pseudo'/>
-            <input type='password' name='password' placeholder='Password'/>
-            <button type='submit'>Login</button>
-        </form>
+        <div className='login-container'>
+            <form onSubmit={handleSubmit}>
+                <h2>Connexion</h2>
+                <div className='login-group'>
+                    <label>Pseudo</label>
+                    <input type='text' name='pseudo' placeholder='Pseudo' required/>
+                </div>
+                <div className='login-group'>
+                    <label>Mot de passe</label>
+                    <input type='password' name='password' placeholder='Mot de passe' required/>
+                </div>
+                <div className='submit-container'>
+                    <button type='submit' className='App-login-btn'>Se connecter</button>
+                    <a href='/signup'>Créer un compte.</a>
+                </div>
+            </form>
+        </div>
     )
 }
 
